fc5d504c7dfaa0d419a29d25f4b98ed067279bf3
Emily-Rose <er.strich@hotmail.fr>
2022-12-16 08:55:19 +0100

    debut de tests

--- libRatio/src/Ratio.cpp
+++ libRatio/src/Ratio.cpp
@@ -5,7 +5,10 @@
 Ratio::Ratio() : m_n((int)0), m_d((unsigned int)1) {
 }
 
-Ratio::Ratio(const int &n, const unsigned int &d) : m_n(n), m_d(d) {
+Ratio::Ratio(const int &n, const unsigned int &d) {
+    m_n = n;
+    m_d = d;
+    reduce();
 }
 
 Ratio::Ratio(const Ratio &ratio) : m_n(ratio.m_n), m_d(ratio.m_d) {
--- myTest/src/test.cpp
+++ myTest/src/test.cpp
@@ -6,3 +6,47 @@
 #include <gtest/gtest.h>
 
 #include "Ratio.hpp"
+
+
+
+/////////////////////////////////////////////////////
+// constructors
+
+TEST (RatioConstructor, defaultContructor) {
+    Ratio ratio;
+    EXPECT_EQ (ratio.getNumerator(), 0);
+    EXPECT_EQ (ratio.getDenominator(), 1);
+}
+
+
+TEST (RatioConstructor, intergersConstructor) {
+    Ratio ratio(9, 5);
+    ASSERT_EQ (ratio.getNumerator(), 9);
+    ASSERT_EQ (ratio.getDenominator(), 5);
+}
+
+
+// TEST (RatioFunctions, reduceFonction) {
+
+// }
+
+
+//reduce
+//constructeur avec n et d, le refaire pour qu'il reduce
+
+
+
+// TEST (RatioConstructor, copyConstructor) {
+//     const size_t maxSize = 100000;  //???? prendre cette taille?
+// 	std::default_random_engine generator(0);
+// 	std::uniform_int_distribution<int> uniformIntDistribution(1,maxSize);
+// 	std::uniform_real_distribution<double> uniformDistributionValue(-int(maxSize),maxSize);
+// 	auto gen = [&uniformDistributionValue, &generator](){ return uniformDistributionValue(generator);};
+
+//     for(int run=0; run<100; ++run) {
+
+//         Ratio ratio1(gen(), gen());
+//     }
+// }
+
+//converttoratio
